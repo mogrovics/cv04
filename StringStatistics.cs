53c207b17471e44bf91cb2ce40fedca69d1313cc
mogrovics <alex.mogrovics@gmail.com>
2021-03-02 11:13:49 +0100

    Add project files.

---
+++ StringStatistics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv04
+{
+    class StringStatistics
+    {
+        public string testString;
+
+        //StringStatistics constructor
+        public StringStatistics(string testString)
+        {
+            this.testString = testString;
+        }
+    }
+}
